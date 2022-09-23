c7d833526f7de68d08442dd8588e891c51a82540
iixin <s6777v@gmail.com>
2022-09-23 10:47:37 +0800

    add groupAPI

--- GroupApi.go
+++ GroupApi.go
@@ -58,3 +58,83 @@ func (c AppClient) GetGroupMembers(groupId string, userIdType UserIdType) []Grou
 	}
 	return all_members
 }
+
+// CreateGroup Create a new group
+func (c AppClient) CreateGroup(groupName string, user_id_type string, owner_id string) *GroupInfo {
+	query := make(map[string]string)
+	query["user_id_type"] = string(user_id_type)
+	body := make(map[string]string)
+	body["name"] = groupName
+	body["owner_id"] = owner_id
+
+	info := c.Request("post", "open-apis/im/v1/chats", query, nil, body)
+
+	return NewGroupInfo(info)
+}
+
+// GetGroupInfo Get a group information
+func (c AppClient) GetGroupInfo(chat_id string) *GroupInfo {
+	info := c.Request("get", "open-apis/im/v1/chats/"+chat_id+"?user_id_type=open_id", nil, nil, nil)
+	info["chat_id"] = chat_id
+	info["tenant_key"] = ""
+	return NewGroupInfo(info)
+}
+
+// AddMembers
+// app_id to add bot
+func (c AppClient) AddMembers(chat_id string, member_id_type string, succeed_type string, id_list []string) bool {
+	query := make(map[string]string)
+	query["member_id_type"] = string(member_id_type)
+	query["succeed_type"] = string(succeed_type)
+
+	body := make(map[string][]string)
+
+	var result bool = true
+	var idlist []string
+
+	for len(id_list) > 50 {
+
+		idlist = id_list[0:50]
+		body["id_list"] = idlist
+		resp := c.Request("post", "open-apis/im/v1/chats/"+chat_id+"/members", query, nil, body)
+		id_list = append(id_list[:0], id_list[50:]...)
+		if resp == nil {
+			result = false
+		}
+	}
+	body["id_list"] = id_list
+	resp := c.Request("post", "open-apis/im/v1/chats/"+chat_id+"/members", query, nil, body)
+	if resp == nil {
+		result = false
+	}
+	return result
+}
+
+// DeleteMembers
+// app_id to delete bot
+func (c AppClient) DeleteMembers(chat_id string, member_id_type string, id_list []string) bool {
+	query := make(map[string]string)
+	query["member_id_type"] = string(member_id_type)
+
+	body := make(map[string][]string)
+
+	var result bool = true
+	var idlist []string
+
+	for len(id_list) > 50 {
+
+		idlist = id_list[0:50]
+		body["id_list"] = idlist
+		resp := c.Request("delete", "open-apis/im/v1/chats/"+chat_id+"/members", query, nil, body)
+		id_list = append(id_list[:0], id_list[50:]...)
+		if resp == nil {
+			result = false
+		}
+	}
+	body["id_list"] = id_list
+	resp := c.Request("delete", "open-apis/im/v1/chats/"+chat_id+"/members", query, nil, body)
+	if resp == nil {
+		result = false
+	}
+	return result
+}

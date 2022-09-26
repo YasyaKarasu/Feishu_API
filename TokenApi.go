6db8ef2375f29e1d2b50313811b32d759e8a461e
iixin <s6777v@gmail.com>
2022-09-26 20:44:18 +0800

    add GetUserAccessToken

--- TokenApi.go
+++ TokenApi.go
@@ -5,11 +5,28 @@ type LoginSession struct {
 	EmployeeId string
 }
 
+type UserAccessToken struct {
+	Access_token  string
+	Name          string
+	Refresh_token string
+	User_id       string
+}
+
 // Create a new LoginSession
 func NewLoginSession(data map[string]interface{}) *LoginSession {
 	return &LoginSession{
 		OpenId:     data["open_id"].(string),
 		EmployeeId: data["employee_id"].(string),
+	}
+}
+
+// Create a new UserAccessToken
+func NewUserAccessToken(data map[string]interface{}) *UserAccessToken {
+	return &UserAccessToken{
+		Access_token:  data["access_token"].(string),
+		Name:          data["name"].(string),
+		Refresh_token: data["refresh_token"].(string),
+		User_id:       data["user_id"].(string),
 	}
 }
 
@@ -23,4 +40,17 @@ func (c AppClient) GetLoginSession(login_token string) *LoginSession {
 		return nil
 	}
 	return NewLoginSession(resp)
+}
+
+// Get the UserAccessToken
+func (c *AppClient) GetUserAccessToken(code string) *UserAccessToken {
+	u := "open-apis/authen/v1/access_token"
+
+	body := make(map[string]string)
+	body["grant_type"] = "authorization_code"
+	body["code"] = code
+
+	resp := c.Request("post", u, nil, nil, body)
+
+	return NewUserAccessToken(resp)
 }

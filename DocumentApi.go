1700ccb354a8a38aa9b3c39966c482bdda59a5e3
YasyaKarasu <oycy0306@163.com>
2022-11-24 14:36:04 +0800

    add GetRawContent function and update Table & Record structure

--- DocumentApi.go
+++ DocumentApi.go
@@ -39,15 +39,19 @@ func (c AppClient) GetAllBitables(DocumentId string) []BitableInfo {
 }
 
 type TableInfo struct {
-	TableId string
-	Name    string
+	AppToken string
+	TableId  string
+	Revision int
+	Name     string
 }
 
 // Create a new TableInfo
-func NewTableInfo(data map[string]interface{}) *TableInfo {
+func NewTableInfo(AppToken string, data map[string]interface{}) *TableInfo {
 	return &TableInfo{
-		TableId: data["table_id"].(string),
-		Name:    data["name"].(string),
+		AppToken: AppToken,
+		TableId:  data["table_id"].(string),
+		Revision: int(data["revision"].(float64)),
+		Name:     data["name"].(string),
 	}
 }
 
@@ -58,20 +62,24 @@ func (c AppClient) GetAllTables(AppToken string) []TableInfo {
 	l := c.GetAllPages("get", "open-apis/bitable/v1/apps/"+AppToken+"/tables", nil, nil, nil, 100)
 
 	for _, value := range l {
-		all_tables = append(all_tables, *NewTableInfo(value.(map[string]interface{})))
+		all_tables = append(all_tables, *NewTableInfo(AppToken, value.(map[string]interface{})))
 	}
 
 	return all_tables
 }
 
 type RecordInfo struct {
+	AppToken string
+	TableId  string
 	RecordId string
 	Fields   map[string]interface{}
 }
 
 // Create a new RecordInfo
-func NewRecordInfo(data map[string]interface{}) *RecordInfo {
+func NewRecordInfo(AppToken string, TableId string, data map[string]interface{}) *RecordInfo {
 	return &RecordInfo{
+		AppToken: AppToken,
+		TableId:  TableId,
 		RecordId: data["record_id"].(string),
 		Fields:   data["fields"].(map[string]interface{}),
 	}
@@ -84,7 +92,7 @@ func (c AppClient) GetAllRecords(AppToken string, TableId string) []RecordInfo {
 	l := c.GetAllPages("get", "open-apis/bitable/v1/apps/"+AppToken+"/tables/"+TableId+"/records", nil, nil, nil, 100)
 
 	for _, value := range l {
-		all_records = append(all_records, *NewRecordInfo(value.(map[string]interface{})))
+		all_records = append(all_records, *NewRecordInfo(AppToken, TableId, value.(map[string]interface{})))
 	}
 
 	return all_records
@@ -94,7 +102,7 @@ func (c AppClient) GetAllRecords(AppToken string, TableId string) []RecordInfo {
 func (c AppClient) GetRecord(AppToken string, TableId string, RecordId string) *RecordInfo {
 	record := c.Request("get", "open-apis/bitable/v1/apps/"+AppToken+"/tables/"+TableId+"/records/"+RecordId, nil, nil, nil)
 
-	return NewRecordInfo(record)
+	return NewRecordInfo(AppToken, TableId, record)
 }
 
 // Get a []Byte form Record by AppToken, TableId and RecordId
@@ -120,4 +128,11 @@ func (c AppClient) GetRecordInByte(AppToken string, TableId string, RecordId str
 	}
 
 	return body
+}
+
+func (c AppClient) GetRawContent(DocumentId string) string {
+	resp := c.Request("get", "open-apis/docx/v1/documents/"+DocumentId+"/raw_content", nil, nil, nil)
+	content := resp["content"].(string)
+
+	return content
 }
